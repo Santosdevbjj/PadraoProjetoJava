89395e50bec3542b2e15b9598a9fa8b87b4a5187
Sergio <santossergiorealbjj@outlook.com>
2025-08-03 21:21:09 -0300

    Create PaymentController.java

---
+++ spring-design-patterns/src/main/java/com/example/designpatterns/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import facade.OrderFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    @Autowired
+    private OrderFacade orderFacade;
+
+    @PostMapping("/{type}/{amount}")
+    public String pay(@PathVariable String type, @PathVariable double amount) {
+        return orderFacade.processOrder(type, amount);
+    }
+}
