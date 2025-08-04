a215a16bedbc0ff49984acf828c7c1bd2e7376c6
Sergio <santossergiorealbjj@outlook.com>
2025-08-04 01:48:54 -0300

    Delete src directory

--- src/strategy/PixPayment.java
+++
@@ -1,8 +0,0 @@
-package strategy;
-
-public class PixPayment implements PaymentStrategy {
-    @Override
-    public void pay(double amount) {
-        System.out.println("Pagamento de R$ " + amount + " via Pix.");
-    }
-}
